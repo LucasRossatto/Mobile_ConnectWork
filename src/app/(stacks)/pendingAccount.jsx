03e1f1e0b09579e90dd21914eb53f2d5302f31ec
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-03-04 22:08:57 -0300

    add new animation on pending account

--- src/app/(stacks)/pendingAccount.jsx
+++ src/app/(stacks)/pendingAccount.jsx
@@ -1,28 +1,64 @@
 import { useRouter } from "expo-router";
-import React from "react";
-import { Text, View, TouchableOpacity } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { Text, View, TouchableOpacity, Animated, Easing, Image } from "react-native";
 
-export default function pendingAccount() {
+export default function PendingAccount() {
   const router = useRouter();
+  const spinValue = useRef(new Animated.Value(0)).current;
+
   const goToLogin = () => {
     router.push("/(stacks)/login");
   };
+
+  useEffect(() => {
+    const spin = Animated.loop(
+      Animated.timing(spinValue, {
+        toValue: 1,
+        duration: 2000,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      })
+    );
+
+    spin.start();
+
+    return () => spin.stop();
+  }, [spinValue]);
+
+  const spin = spinValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: ["0deg", "360deg"],
+  });
+
   return (
-    <View className="flex-1 flex justify-center bg-backgroundDark p-4">
+    <View className="flex-1 justify-center bg-backgroundDark p-4">
+      <View className="items-center mb-6">
+        <Animated.View style={{ transform: [{ rotate: spin }] }}>
+          <Image
+            source={require("../../../assets/images/ampulheta.png")}
+            className="w-[54] h-[54]"
+          />
+        </Animated.View>
+      </View>
+
       <Text className="text-4xl text-white font-medium mb-4 text-center">
         Conta Solicitada!
       </Text>
+
       <View className="flex justify-center items-center">
-        <Text className="text-xl text-gray-300 font-normal  mb-10 text-center w-[70%]">
+        <Text className="text-xl text-gray-300 font-normal mb-10 text-center w-[70%]">
           Aguarde um administrador aceitar sua conta!
         </Text>
       </View>
-      <TouchableOpacity
-        className="bg-white py-5 px-10 rounded-[14]"
-        onPress={goToLogin}
-      >
-        <Text className="text-black text-lg">Ir para a tela de login</Text>
-      </TouchableOpacity>
+
+      <View className="absolute bottom-10 left-0 right-0 px-5 mx-4">
+        <TouchableOpacity
+          className="bg-white py-5 rounded-[14]"
+          onPress={goToLogin}
+        >
+          <Text className="text-black text-lg text-center">Ir para a tela de login</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
