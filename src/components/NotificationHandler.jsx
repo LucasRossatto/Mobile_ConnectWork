d2ff3924f12d19ee9af6999c674d4b7dda1da090
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-05-28 23:37:27 -0300

    delete inutil file

--- src/components/NotificationHandler.jsx
+++
@@ -1,6 +0,0 @@
-import { useEffect, useRef } from "react";
-import * as Notifications from "expo-notifications";
-import { Platform } from "react-native";
-import { useNotifications } from "@/contexts/NotificationContext";
-import { useRouter } from "expo-router";
-
